532aeb2a4f16675ce64fbfa7a02dcfbe511d922f
tom981105-web <tom981105@gmail.com>
2026-03-28 12:15:12 +0900

    Standardize CSP headers across app pages

--- server.js
+++ server.js
@@ -23,6 +23,21 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '150mb' }));
 
+app.use((req, res, next) => {
+    res.setHeader(
+        'Content-Security-Policy',
+        [
+            "default-src 'self' https: data: blob:",
+            "script-src 'self' https: 'unsafe-inline'",
+            "style-src 'self' https: 'unsafe-inline'",
+            "img-src 'self' https: data: blob:",
+            "font-src 'self' https: data:",
+            "connect-src 'self' https: http://localhost:3000"
+        ].join('; ')
+    );
+    next();
+});
+
 ensureDataLayout();
 
 app.use('/uploads', express.static(UPLOADS_DIR));
